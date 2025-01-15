06b5efccd167f0fdd1917c547b70a95beeb0cac0
Eamon <c3046478@newcastle.ac.uk>
2025-01-15 22:57:02 +0000

    added README.md and main.py. created basic program structure

---
+++ README.md
@@ -0,0 +1,4 @@
+# Pomodoro Timer
+
+This is a simple Pomodoro Timer Application.
+<br> Users can use this app to assist them in studying.
---
+++ main.py
@@ -0,0 +1,32 @@
+import time
+import tkinter as tk
+
+WORK_TIME = 25 * 60  # 25 minutes
+SHORT_BREAK_TIME = 5 * 60  # 5 minutes
+LONG_BREAK_TIME = 15 * 60  # 15 minutes
+
+
+class Pomodoro:
+    def __init__(self, root):
+        self.root = root
+
+    def format_time(self):
+        return
+
+    def start_timer(self):
+        return
+
+    def pause_timer(self):
+        return
+
+    def reset_timer(self):
+        return
+
+    def count_down(self):
+        return
+
+
+# Run app
+root = tk.Tk()
+app = Pomodoro(root)
+root.mainloop()
